5e05a30d8771db494f2390d02950825e59eedc73
dreamtraveller1314 <xinru2509@gmail.com>
2025-05-25 23:06:07 +0800

    uhh testing onlyyy

--- src/main/java/com/dreamtraveller/blinkablemod/BlinkableEyeDataGenerator.java
+++ src/main/java/com/dreamtraveller/blinkablemod/BlinkableEyeDataGenerator.java
@@ -3,6 +3,7 @@ package com.dreamtraveller.blinkablemod;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
+//why Im writing thissss
 public class BlinkableEyeDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
