4cd4ac728e6b7f3a6d8434ca2d1d612d91da88ab
dreamtraveller1314 <xinru2509@gmail.com>
2025-05-28 22:24:14 +0800

    Add first blockyyyy

--- src/main/java/com/dreamtraveller/blinkablemod/BlinkableEye.java
+++ src/main/java/com/dreamtraveller/blinkablemod/BlinkableEye.java
@@ -1,5 +1,6 @@
 package com.dreamtraveller.blinkablemod;
 
+import com.dreamtraveller.blinkablemod.block.ModBlocks;
 import com.dreamtraveller.blinkablemod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
@@ -13,5 +14,6 @@ public class BlinkableEye implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
---
+++ src/main/java/com/dreamtraveller/blinkablemod/block/ModBlocks.java
@@ -0,0 +1,38 @@
+package com.dreamtraveller.blinkablemod.block;
+
+import com.dreamtraveller.blinkablemod.BlinkableEye;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+
+public class ModBlocks {
+
+    public static final Block BLINKING_EYE_BLOCK = registerBlock("blinking_eye_block",
+            new Block(AbstractBlock.Settings.create().strength(4f)));
+
+    private static Block registerBlock(String name, Block block){
+        registerBlockItem(name, block);
+        return Registry.register(Registries.BLOCK, Identifier.of(BlinkableEye.MOD_ID, name), block);
+    }
+
+    private static void registerBlockItem(String name, Block block){
+        Registry.register(Registries.ITEM, Identifier.of(BlinkableEye.MOD_ID, name),
+                new BlockItem(block, new Item.Settings()));
+    }
+
+    public static void registerModBlocks() {
+        BlinkableEye.LOGGER.info("Registering Mod Blocks for " + BlinkableEye.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> {
+            entries.add(ModBlocks.BLINKING_EYE_BLOCK);
+        }) ;
+    }
+
+}
