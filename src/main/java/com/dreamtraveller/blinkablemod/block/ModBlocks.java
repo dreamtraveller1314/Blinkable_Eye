4bcc73913b226e86b217a91504eac32d2b6bc210
dreamtraveller1314 <xinru2509@gmail.com>
2025-05-29 01:44:13 +0800

    Special Effect when right click the block Blinking Eye Items on the block change to diamond

--- src/main/java/com/dreamtraveller/blinkablemod/block/ModBlocks.java
+++ src/main/java/com/dreamtraveller/blinkablemod/block/ModBlocks.java
@@ -1,10 +1,10 @@
 package com.dreamtraveller.blinkablemod.block;
 
 import com.dreamtraveller.blinkablemod.BlinkableEye;
+import com.dreamtraveller.blinkablemod.block.custom.Blinking_Eye_Block;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
-import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -12,12 +12,11 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.math.intprovider.UniformIntProvider;
 
 public class ModBlocks {
 
     public static final Block BLINKING_EYE_BLOCK = registerBlock("blinking_eye_block",
-            new Block(AbstractBlock.Settings.create().strength(3f).sounds(BlockSoundGroup.CHERRY_WOOD)));
+            new Blinking_Eye_Block(AbstractBlock.Settings.create().strength(3f).sounds(BlockSoundGroup.CHERRY_WOOD)));
 
     private static Block registerBlock(String name, Block block){
         registerBlockItem(name, block);
