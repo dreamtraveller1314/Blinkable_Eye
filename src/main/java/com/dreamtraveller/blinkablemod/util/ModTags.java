69b1e7ed421619184201b1ea87dbf38b77b048a3
dreamtraveller1314 <xinru2509@gmail.com>
2025-05-30 21:20:39 +0800

    util

---
+++ src/main/java/com/dreamtraveller/blinkablemod/util/ModTags.java
@@ -0,0 +1,26 @@
+package com.dreamtraveller.blinkablemod.util;
+
+import com.dreamtraveller.blinkablemod.BlinkableEye;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
+import net.minecraft.block.Block;
+
+public class ModTags {
+    public static class Item {
+        public static final TagKey<Item> EYE_ITEMS = createTag("eye_items");
+
+        private static TagKey<Item> createTag(String name){
+            return TagKey.of((net.minecraft.registry.RegistryKey<? extends net.minecraft.registry.Registry<Item>>) RegistryKeys.ITEM, Identifier.of(BlinkableEye.MOD_ID, name));
+        }
+    }
+
+    public static class Blocks{
+
+        public static final TagKey<Block> EYE_BLOCKS = createTag("eye_blocks");
+
+        private static TagKey<Block> createTag(String name){
+            return TagKey.of(RegistryKeys.BLOCK, Identifier.of(BlinkableEye.MOD_ID, name));
+        }
+    }
+}
